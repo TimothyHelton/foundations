58507ef0150e3e78101ad7eea822423055ae12b7
Timothy Helton <timothy.j.helton@gmail.com>
2016-12-08 21:55:14 -0700

    FEAT: add selective sort

---
+++ foundations/__init__.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pkg_resources import get_distribution, DistributionNotFound
+import os.path as osp
+
+from . import sort
+
+
+try:
+    _dist = get_distribution('foundations')
+    dist_loc = osp.normcase(_dist.location)
+    here = osp.normcase(__file__)
+    if not here.startswith(osp.join(dist_loc, 'foundations')):
+        raise DistributionNotFound
+except DistributionNotFound:
+    __version__ = 'Please install this project with setup.py'
+else:
+    __version__ = _dist.version
--- foundations/sort.py
+++ foundations/sort.py
@@ -6,5 +6,82 @@
 .. moduleauthor:: Timothy Helton <timothy.j.helton@gmail.com>
 """
 
+import logging
+import random
+
+from strumenti import system
+
+
+logger = system.logger_setup(name=__name__, master_level=logging.WARNING)
+
+
+class Sorting:
+    """Sorting Algorithms
+
+    :param list items: sequence of items to be sorted
+
+    :Attributes:
+
+        - **items**: *list* items to be sorted
+        - **item_length**: *int* length of the items list
+    """
+    def __init__(self, items=None):
+        self.items = items
+        self._item_length = None
+
+    @property
+    def item_length(self):
+        try:
+            self._item_length = len(self.items)
+        except TypeError:
+            pass
+        return self._item_length
+
+    def __repr__(self):
+        return 'Sorting(items={})'.format(self.items)
+
+    def selection_sort(self):
+        """Sorts items into ascending order using the selection sort algorithm.
+
+        :returns: items sorted from smallest to largest value
+        :rtype: list
+        """
+        logger.debug('execute: selection_sort')
+        if not self.items:
+            logger.warning('No items to sort were provided.')
+            return None
+
+        for idx in range(self.item_length):
+            min_loc = idx
+            for loc in range(idx, self.item_length):
+                if self.items[loc] < self.items[min_loc]:
+                    min_loc = loc
+            if min_loc is idx:
+                continue
+            else:
+                # self.items.insert(idx, self.items.pop(min_loc))
+                old_value = self.items[idx]
+                self.items[idx] = self.items[min_loc]
+                self.items[min_loc] = old_value
+        logger.debug(self.items)
+
+
 if __name__ == '__main__':
-    pass
+    solution_methods = ('selection_sort',
+                        )
+    test_items = random.sample(range(100), 100)
+    # test_items = [4, 3, 2, 1]
+    # test_items = [4, 3, 3, 2]
+
+    sort = Sorting(items=test_items)
+    sort.selection_sort()
+
+    from timeit import timeit
+    iterations = int(10e3)
+    times = {x: timeit('calc = Sorting(items={}); calc.{}()'.format(test_items,
+                                                                    x),
+                       'from __main__ import Sorting', number=iterations)
+             for x in solution_methods}
+
+    for sol in sorted(solution_methods, key=lambda x: times[x]):
+        logger.info('{:30}{}'.format(sol + ':', times[sol]))
